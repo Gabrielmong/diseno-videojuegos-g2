4a8af1f3327c09bad4216c1b7747feb9c0b3cc1a
Gabrielmong <gabriel.monge.lizano@gmail.com>
2023-03-09 20:25:56 -0600

    feat: dynamic grid

--- Assets/Scripts/GameGrid.cs
+++ Assets/Scripts/GameGrid.cs
@@ -10,6 +10,12 @@ public class GameGrid : MonoBehaviour
 
     public GameObject grass;
 
+    [SerializeField]
+    private GameObject flowers;
+
+    [SerializeField]
+    private int flowerChance = 10;
+
     public GameObject[] currentGrid;
 
     public bool gotGrid;
@@ -25,37 +31,71 @@ public class GameGrid : MonoBehaviour
 
     void Start()
     {
-        for(int i=0; i<columnLength*rowLength; i++)
+        for (int i = 0; i < columnLength * rowLength; i++)
         {
-            Instantiate(grass, new Vector3(x_Space +(x_Space * (i %columnLength)), 0, z_Space +(z_Space *(i/columnLength))), Quaternion.identity);
+            int randInt = Random.Range(0, 100);
+
+            if (randInt < flowerChance)
+            {
+                Instantiate(flowers, new Vector3(x_Space + (x_Space * (i % columnLength)), 0, z_Space + (z_Space * (i / columnLength))), Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(grass, new Vector3(x_Space + (x_Space * (i % columnLength)), 0, z_Space + (z_Space * (i / columnLength))), Quaternion.identity);
+            }
         }
     }
 
     void Update()
     {
-        if(gotGrid == false)
+        if (gotGrid == false)
         {
             currentGrid = GameObject.FindGameObjectsWithTag("grid");
-            gotGrid = true; 
+            gotGrid = true;
         }
 
-        if( Input.GetMouseButtonDown (0))
+        if (Input.GetMouseButtonDown(0))
         {
-            
-            if(Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out _Hit))
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out _Hit))
             {
+
+                Debug.Log(_Hit.transform.gameObject.name);
                 if (creatingFields == true)
                 {
-                    if(_Hit.transform.tag == "grid")
+                    if (_Hit.transform.tag == "grid"
+                        && (_Hit.transform.gameObject.name == "Flowers(Clone)"
+                        || _Hit.transform.gameObject.name == "Grass(Clone)"))
                     {
                         hitted = _Hit.transform.gameObject;
-                        Instantiate(field,hitted.transform.position, Quaternion.identity);
+                        Instantiate(field, hitted.transform.position, Quaternion.identity);
                         Destroy(hitted);
                     }
+                    else if (_Hit.transform.tag == "grid"
+                        && _Hit.transform.gameObject.name == "Field(Clone)")
+                    {
+                        Debug.Log("Field");
+                        hitted = _Hit.transform.gameObject;
+                        Instantiate(grass, hitted.transform.position, Quaternion.identity);
+                        Destroy(hitted);
 
+                    }
                 }
-            
-             }
+
+            }
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out _Hit))
+            {
+                if (_Hit.transform.tag == "grid"
+                    && _Hit.transform.gameObject.name == "Field(Clone)")
+                {
+                    hitted = _Hit.transform.gameObject;
+                    Instantiate(flowers, hitted.transform.position, Quaternion.identity);
+                    Destroy(hitted);
+                }
+            }
         }
     }
 
